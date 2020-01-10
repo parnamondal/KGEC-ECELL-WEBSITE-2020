0a86ce4370bf40390c8ac024fd13fdfc2635ecbd
Parna Mondal <ayushimondal790@gmail.com>
2020-01-10 23:25:14 +0530

    up

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   // Preloader
   $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
+      $('#preloader').delay(50).fadeOut('slow', function () {
         $(this).remove();
       });
     }
